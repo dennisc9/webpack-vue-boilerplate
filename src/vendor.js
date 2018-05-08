c2efcd3dc07f7f44fe81c1f9de577f9d659fb24d
Dennis Climent <dennis@denniscliment.com>
2018-05-08 16:12:56 -0400

    working version

--- src/vendor.js
+++ src/vendor.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
 import "lodash";
 
-import "babel-polyfill"; // This is to make Object.assign work in IE
-import "event-source-polyfill"; // for IE hot module replacement because its IE
+// import "babel-polyfill"; // This is to make Object.assign work in IE
+// import "event-source-polyfill"; // for IE hot module replacement because its IE
